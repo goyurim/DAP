379dd47cced77827c28565b5bc75b35421769207
goyurim <qpeijty1@naver.com>
2021-04-13 18:22:54 +0900

    Update shape_classification.py

--- DAP-Server/dap_api/shape_classification.py
+++ DAP-Server/dap_api/shape_classification.py
@@ -28,8 +28,8 @@ def primaryModel():
     model = Sequential()
     model.add(Conv2D(32, kernel_size=(3, 3),
                      activation='relu',
-                     input_shape=(24, 24, 3)))  # 32%,
-    model.add(Conv2D(64, (3, 3), activation='relu'))  # 32%, 36%
+                     input_shape=(24, 24, 3)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
